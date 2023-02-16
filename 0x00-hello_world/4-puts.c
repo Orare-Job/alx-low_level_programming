16895bba8c354e9720bbb0b9eda6d29a84eca109
Job Onduko Orare <ondukojob@gmail.com>
2023-02-16 11:44:41 -0800

    q.4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- *  main: entrance point
+ *  main(): entrance point
  * return 0: on success
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzl.\n");
+	puts("Programming is like building a multilingual puzzle.\n");
 	return (0);
 }
