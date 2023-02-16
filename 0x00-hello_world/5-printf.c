329bc21767b8105ba05786d959b95ece7299e448
Job Onduko Orare <ondukojob@gmail.com>
2023-02-16 12:25:18 -0800

    q.5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * int main: describes entrance point
  * return (0):on success
  */
-int main (void)
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
