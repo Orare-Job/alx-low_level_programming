f1287e529526dec567039da69eaa72624af5c93f
Job Onduko Orare <ondukojob@gmail.com>
2023-02-17 14:10:24 -0800

    q.4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 /**
- * main - print out Programming is like building a multilingual puzzle
- * return (0): On success
+ * main - entry point
+ *
+ * return: Always 0 (Success)
  */
 int main(void)
 {
-	puts(""Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
