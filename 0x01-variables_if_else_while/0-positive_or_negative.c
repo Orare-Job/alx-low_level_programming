3ee09cc366508239a6636fb6be79ecd354c498ef
Job Onduko Orare <ondukojob@gmail.com>
2023-02-18 13:33:25 -0800

    q.0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,7 +8,7 @@
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
 	int n;
 
@@ -20,6 +20,5 @@ int main (void)
 	  printf("%d is negative\n", n);
 	else
 	  printf("%d is zero\n", n);
-	
 	return (0);
 }
