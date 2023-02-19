82a360b213a126da4c47fed6ae9fca4d7f28774e
Job Onduko Orare <ondukojob@gmail.com>
2023-02-18 17:18:08 -0800

    q.5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	x = 0;
+	do {
+		printf("%d \n", x);
+		x++;
+	} while (x <= 10);
+	return (0);
+}
