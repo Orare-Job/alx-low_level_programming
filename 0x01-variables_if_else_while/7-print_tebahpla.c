17d281126173bef2226bdfe8d1142f5834d95e4a
Job Onduko Orare <ondukojob@gmail.com>
2023-02-19 00:32:59 -0800

    q.7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 on Success
+ */
+
+int main(void)
+{
+	char z;
+
+	z = 'z';
+	while (z >= 'a')
+	{
+		putchar(z);
+		z--;
+	}
+	putchar('\n');
+	return (0);
+}
