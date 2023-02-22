e17483f3433947ce8afdd891be483d885e9ee554
Job Onduko Orare <ondukojob@gmail.com>
2023-02-22 03:10:06 -0800

    q.10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @x: First integer
+ * @y: Second integer
+ * Return: Result of the added numbers (integer)
+ */
+
+int add(int x, int y)
+{
+	int result = x + y;
+
+	return (result);
+}
