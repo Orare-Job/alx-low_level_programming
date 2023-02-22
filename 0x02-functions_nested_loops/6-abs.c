35f79b5116c0062478c69f697f11eb85c2691f0d
Job Onduko Orare <ondukojob@gmail.com>
2023-02-21 17:31:16 -0800

    q.6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer
+ * @r: the integer whose absoute value is computed
+ * Return: Absolute value
+ */
+
+int _abs(int r)
+{
+	if (r > 0)
+	{
+		return (r * 1);
+	}
+	else
+	{
+		return (r * -1);
+	}
+}
