75e6ed734951044f13f21502c60e157c55fdbde6
Job Onduko Orare <ondukojob@gmail.com>
2023-02-22 04:28:16 -0800

    q.11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,11 +10,17 @@ void print_to_98(int n)
 	for (n = 0; n <= 98; n++)
 	{
 		printf("%d, ", n);
+		if (n == 98)
+			continue;
+		printf(" \n");
 	}
 
 	for (n = 0; n >= 98; n--)
 	{
-		printf("%d, ", n);
+		printf("%d", n);
+		if (n == 98)
+			continue;
+		printf(" \n");
 	}
 
 	putchar('\n');
