b9d95e9b826a3106b02a0079ae7ed16ec6d2b544
Job Onduko Orare <ondukojob@gmail.com>
2023-02-23 13:40:16 -0800

    q.2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int z;
+
+	z = a * b;
+
+	return (0);
+}
