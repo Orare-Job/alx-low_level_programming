888717378687c65897cf5b59100ca5b779273b31
Job Onduko Orare <ondukojob@gmail.com>
2023-02-23 14:18:49 -0800

    q.4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9
+ * Do not print 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar((i % 10) + '0');
+		i++;
+		if (i == 2 && i == 4 && i == 9)
+			continue;
+	}
+	_putchar('\n');
+}
