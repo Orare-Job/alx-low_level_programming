6299b2aec5bcfa4249f8271abbeefece2d5c7908
Job Onduko Orare <ondukojob@gmail.com>
2023-02-27 13:46:27 -0800

    q.2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: The string whose length is returned
+ * Return: The length of the provided string
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(*s);
+
+	return (len);
+}
