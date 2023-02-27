900c4e202f91d031f7ab536182b73db3e1a4295b
Job Onduko Orare <ondukojob@gmail.com>
2023-02-27 13:08:46 -0800

    c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * reset_to_98 - Takes a pointer to an int as parameter and updates the value it points
+ * reset_to_98 - Takes a pointer to an int as parameter
+ *           and updates the value it points
  *
  * @n: pointer, pointing to an int
  */
 
 void reset_to_98(int *n)
 {
-	int *n = 98
+	 *n = 98;
 }
