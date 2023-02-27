c81607811ce283f1d7259dbbf0d07b2812325aba
Job Onduko Orare <ondukojob@gmail.com>
2023-02-27 14:59:19 -0800

    q.4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,10 +12,10 @@ void print_rev(char *s)
 	while (s[index++])
 	{
 		len++;
-	}
-	for (index = len - 1; index > 0; index--)
-	{
-		putchar(s[index]);
+		for (index = len - 1; index > 0; index--)
+		{
+			putchar(s[index]);
+		}
 	}
 	putchar('\n');
 }
