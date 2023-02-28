31273fd4f24484612efc95d92670ec54dd09c52d
Job Onduko Orare <ondukojob@gmail.com>
2023-02-28 15:34:03 -0800

    p

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: String to print half
+ */
+
+void puts_half(char *str)
+{
+	int i, len1, len2;
+
+	len1 = 0;
+	while (*str++)
+	{
+		len1++;
+	}
+	if (len1 % 2 == 0)
+		len2 = len1 / 2;
+	else
+		len2 = (len1 + 1) / 2;
+	for (i = 0; i <= len2; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
