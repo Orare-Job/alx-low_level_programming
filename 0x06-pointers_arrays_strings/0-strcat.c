c7fd5c31649bf23cd6a7429cdfe4497da3458469
Job Onduko Orare <ondukojob@gmail.com>
2023-03-02 05:12:32 -0800

    s

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,15 +6,25 @@
  * @src: Second string to concatnate
  * Return: a pointer to the resulting string dest
  */
-char *strcat(char *dest, const char *src)
+
+char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int i, length1, length2;
 
-	while (dest[index++])
-		dest_len++;
+	length1 = 0;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	while (*(dest++))
+		length1++;
+
+	length2 = 0;
+
+	while (*(src++))
+		length2++;
+
+	for (i = 0; i <= length2; i++)
+	{
+		dest[length1 + i] = src[i];
+	}
 
 	return (dest);
 }
