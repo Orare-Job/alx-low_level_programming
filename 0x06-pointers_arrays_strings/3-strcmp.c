793fbc0937fa7f488b6d44d35147de7d0e35c977
Job Onduko Orare <ondukojob@gmail.com>
2023-03-02 13:53:03 -0800

    s

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,10 +12,10 @@ int _strcmp(char *s1, char *s2)
 	char *ss1 = s1;
 	char *ss2 = s2;
 
-	while (*ss1 != '\0' && ss2 != '\0' && ss1 == ss2)
+	while (*ss1 != '\0' && *ss2 != '\0' && *ss1 == *ss2)
 	{
-		*ss1++;
-		*ss2++;
+		ss1++;
+		ss2++;
 	}
 	return (*ss1 - *ss2);
 }
