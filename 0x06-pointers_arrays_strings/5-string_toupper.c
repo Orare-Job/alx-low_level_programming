0db6eaed10944e5677ea264a5ce4deba4536ebd0
Job Onduko Orare <ondukojob@gmail.com>
2023-03-02 14:49:08 -0800

    s

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - Changes all lowercase letter
+ *     of a string to uppercase
+ * @str: String changed to upper case
+ * Return: A pointer to the changed string
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0' i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = (int)stri[i] - 32;
+	}
+	return (str);
+}
+
