085e39d8c5017fd3b763c6f32bbb773913793168
Job Onduko Orare <ondukojob@gmail.com>
2023-03-02 15:00:44 -0800

    w

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -14,7 +14,7 @@ char *string_toupper(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] = (int)stri[i] - 32;
+			str[i] = str[i] - 32;
 	}
 	return (str);
 }
