a393b9356e0d9e9eeffe15fb93d68981d39977cc
Job Onduko Orare <ondukojob@gmail.com>
2023-03-02 03:51:57 -0800

    q.0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: First string to concatnate
+ * @src: Second string to concatnate
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+
+	while (*dest++)
+		i++;
+
+	j = 0 + i;
+
+	while (*src++)
+		j++;
+	dest[j];
+
+	return (dest);
+}
