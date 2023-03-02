e08e0ea49533186e4f82dbd88ded57b6d1e2797a
Job Onduko Orare <ondukojob@gmail.com>
2023-03-02 10:36:57 -0800

    q

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,20 +7,18 @@
  * @n: Maximum number of bytes to copy from src
  * Return: a pointer to the resulting string dest
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	while (src[i] != '\0')
+	i = 0;
+
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
-
-	while (i < n)
-		dest[i] = '\0';
-		i++;
+	dest[i] = '\0';
 
 	return (dest);
 }
