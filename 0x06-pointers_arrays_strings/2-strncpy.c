0f5a132821d30fb39f40c909bf15d97e2e17db3c
Job Onduko Orare <ondukojob@gmail.com>
2023-03-02 07:35:58 -0800

    s

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies a string
+ * @dest: Destination string is copied to
+ * @src: Source string is copied from
+ * @n: Maximum number of bytes to copy from src
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+		dest[i] = src[i];
+	_putchar('\n');
+}
