97ed750868bb83f0ea9cbaa0397511ca0d574f79
Job Onduko Orare <ondukojob@gmail.com>
2023-03-02 12:33:35 -0800

    s

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,22 +9,22 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i, length1, length2;
+	int i, j;
 
-	length1 = 0;
-
-	while (*dest++)
-		length1++;
-
-	length2 = 0;
-
-	while (*src++)
-		length2++;
-
-	for (i = 0; i <= length2; i++)
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		dest[length1 + i] = src[i];
+		i++;
 	}
+
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 
 	return (dest);
 }
