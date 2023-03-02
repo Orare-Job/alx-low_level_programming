7f2b2b3b72cb8c8edb50ce18fe4fa7eb03726ec6
Job Onduko Orare <ondukojob@gmail.com>
2023-03-02 06:47:17 -0800

    d

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings
+ * @dest: String to concatnate and as destination
+ * @src: String to concatnate and as source
+ * @n: Maxmum byte from src sting
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len1, len2, i;
+
+	len1 = 0;
+
+	while (*(dest++))
+		len1++;
+
+	len2 = 0;
+
+	while (*(src++))
+		len2++;
+
+	for (i = 0; i <= len2; i++)
+	{
+		if (i <= n)
+			dest[len1 + i] = src[i];
+	}
+
+	return (dest);
+}
