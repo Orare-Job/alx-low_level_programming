cde33d8546aa0c3fe086a8426e59b55e580b3176
Job Onduko Orare <ondukojob@gmail.com>
2023-03-02 16:38:43 -0800

    sd

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,19 +8,30 @@
 
 char *cap_string(char *str)
 {
-	int i = 0;
+	int index = 0;
 
-	while (str[i] != '\0')
+	while (str[index] != '\0')
 	{
-		if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
-			str[i - 1] == ',' str[i - 1] == ';' || str[i - 1] == '.' ||
-			str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"'
-			|| str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{'
-			|| str[i - 1] == '}' || i == 0)
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
 		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-				str[i] -= 32;
-			i++
+			if (str[index] >= 'a' && str[index] <= 'z')
+			{
+				str[i] = str[i] - 32;
+				index++;
+			}
 		}
 	}
 	return (str);
